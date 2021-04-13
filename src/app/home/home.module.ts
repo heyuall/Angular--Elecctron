c12c93c00e26f07829640504f4bd0709329a45c2
heyuall <zoubeidihoussem@gmail.com>
2021-04-13 23:40:17 +0100

    Fix home-routing.module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,9 +5,25 @@ import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { TodoComponent } from './todo/todo.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  declarations: [HomeComponent, TodoComponent],
+  imports: [CommonModule, SharedModule, HomeRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    DragDropModule,
+  ],
+  exports: [TodoComponent]
 })
 export class HomeModule {}
